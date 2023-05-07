58a0d53789e2a73b55f766d8ca3164d869255c93
seanoh1014 <ohsean1014@gmail.com>
2023-05-06 21:08:42 -0500

    edit dwm

--- home-manager/dwm.nix
+++ home-manager/dwm.nix
@@ -27,9 +27,9 @@ let
      url = "https://github.com/seanoh1014/dwmblocks-nix2/tarball/master";
      sha256 = "0qk41bjvaj6hg59ypyp91g5ifijssdwzwjbcsaflqgsks456pfdd";
   };
-  #patches = [
-  #  ./patches/dwmblocks-statuscmd.diff
-  #];
+  patches = [
+    ./patches/dwmblocks-statuscmd.diff
+  ];
   });
 
 in
