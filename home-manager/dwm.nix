436f4ebbcedb68c09cf0137213eb5f000fc3fe25
seanoh1014 <ohsean1014@gmail.com>
2023-05-13 12:07:46 -0500

    edit dwm

--- home-manager/dwm.nix
+++ home-manager/dwm.nix
@@ -24,7 +24,7 @@ let
 
   dwmblocks = pkgs.dwmblocks.overrideAttrs (oldAttrs: rec {
      src = builtins.fetchTarball {
-     url = "https://github.com/seanoh1014/dwmblocks-unpatched/tarball/master";
+     url = "https://github.com/seanoh1014/dwmblocks-torrinfail/tarball/master";
      sha256 = "1cw78y7i9373a0s3i7ny1b45lizq9b4hxrrqhvrra8ayl4fmqrcm";
   };
   patches = [
