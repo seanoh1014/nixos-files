df460ed58a15cfd5161f6eeeae5ce1f1211368aa
seanoh1014 <ohsean1014@gmail.com>
2023-12-22 12:42:40 -0600

    edit config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -31,6 +31,8 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
+  i18n.supportedLocales = ["all"];
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
