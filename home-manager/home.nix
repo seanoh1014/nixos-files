a3ab9b3834283dc43d7c32c260f99880ed09d17e
seanoh1014 <ohsean1014@gmail.com>
2024-01-06 01:06:55 -0600

    edit home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -121,6 +121,7 @@
     ".config/mpv/scripts/modern.lua".source = dotfiles/mpv/modern.lua;
     ".config/mpv/fonts/Material-Design-Iconic-Font.ttf".source = dotfiles/mpv/Material-Design-Iconic-Font.ttf;
     ".config/mpv/mpv.conf".source = dotfiles/mpv/mpv.conf;
+    # doom emacs
     #"./doom.d/config.el".source = dotfiles/doom/config.el;
     #"./doom.d/init.el".source = dotfiles/doom/init.el;
     #"./doom.d/packages.el".source = dotfiles/doom/packages.el;
