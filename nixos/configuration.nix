10b2a3d2cfd1780132fdc52a55e4a21edc934f1b
seanoh1014 <ohsean1014@gmail.com>
2025-03-09 01:50:35 -0600

    update config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -148,9 +148,9 @@
   i18n = {
     inputMethod = {
       enable = true;
-      #type = "kime";
-      type = "ibus";
-      ibus.engines = with pkgs.ibus-engines; [ hangul ];
+      type = "kime";
+      #type = "ibus";
+      #ibus.engines = with pkgs.ibus-engines; [ hangul ];
     };
   };
 
