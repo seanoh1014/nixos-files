a4927e9753130827a10365341e7b825f88b86544
seanoh1014 <ohsean1014@gmail.com>
2026-04-01 02:04:22 -0500

    update config

--- home-manager/dwm.nix
+++ home-manager/dwm.nix
@@ -1,14 +1,14 @@
 { config, pkgs, lib, ... }:
 let
-  dwm = pkgs.dwm.overrideAttrs (old: {
-    src = pkgs/dwm;
-    nativeBuildInputs = with pkgs; [ #writing once works for both currently, sort of bug and feature
-      xorg.libX11.dev
-      xorg.libXft
-      imlib2
-      xorg.libXinerama
-    ];
-  });
+#  dwm = pkgs.dwm.overrideAttrs (old: {
+#    src = pkgs/dwm;
+#    nativeBuildInputs = with pkgs; [ #writing once works for both currently, sort of bug and feature
+#      xorg.libX11.dev
+#      xorg.libXft
+#      imlib2
+#      xorg.libXinerama
+#    ];
+#  });
 
   dmenu = pkgs.dmenu.overrideAttrs (old: {
     src = pkgs/dmenu-5.3;
