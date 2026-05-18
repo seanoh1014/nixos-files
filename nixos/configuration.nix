5ea4aed4360cf4d425e3c7b72503258c5ac84c6c
seanoh1014 <ohsean1014@gmail.com>
2026-05-17 23:32:24 -0500

    update config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -179,14 +179,14 @@
   #dns
   services.resolved = {
     enable = false;
-#    settings = ''
-#      [Resolve]
-#      DNS=45.90.28.0#31c977.dns.xnextdn.io
-#      DNS=2a07:a8c0::#31c977.dns.nextdns.io
-#      DNS=45.90.30.0#31c977.dns.nextdns.io
-#      DNS=2a07:a8c1::#31c977.dns.nextdns.io
-#      DNSOverTLS=yes
-#    '';
+    settings.Resolve.DNS = ''
+      [Resolve]
+      DNS=45.90.28.0#31c977.dns.xnextdn.io
+      DNS=2a07:a8c0::#31c977.dns.nextdns.io
+      DNS=45.90.30.0#31c977.dns.nextdns.io
+      DNS=2a07:a8c1::#31c977.dns.nextdns.io
+      DNSOverTLS=yes
+    '';
   #  dnsovertls = "true";
   #  dnssec = "true";
   };
