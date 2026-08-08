9245ce089ab44ee72df203562d6e0be71301bc50
seanoh1014 <ohsean1014@gmail.com>
2026-08-08 16:09:56 -0500

    fix niri workspace indicator proportions

--- home-manager/niri/default.nix
+++ home-manager/niri/default.nix
@@ -296,7 +296,11 @@ in
       modules-right = [ "network" "pulseaudio" "backlight" "battery" "clock" ];
 
       "niri/workspaces" = {
-        format = "";
+        format = "{icon}";
+        format-icons = {
+          default = "●";
+          active = "▬";
+        };
         on-click = "activate";
       };
 
@@ -367,28 +371,28 @@ in
 
       #workspaces button {
         min-width: 7px;
-        min-height: 7px;
-        margin: 7px 4px;
-        padding: 0;
-        border-radius: 4px;
-        background: #f8f8f2;
+        padding: 0 4px;
+        background: transparent;
+        color: #f8f8f2;
         box-shadow: none;
-        transition: min-width 180ms ease-in-out, background-color 180ms ease-in-out;
+        transition: min-width 180ms ease-in-out, color 180ms ease-in-out;
       }
 
       #workspaces button.active {
         min-width: 22px;
-        background: #8be9fd;
+        background: transparent;
+        color: #8be9fd;
       }
 
       #workspaces button:hover {
-        background: #f8f8f2;
+        background: transparent;
         box-shadow: none;
         text-shadow: none;
       }
 
       #workspaces button.active:hover {
-        background: #8be9fd;
+        background: transparent;
+        color: #8be9fd;
       }
 
       #network,
