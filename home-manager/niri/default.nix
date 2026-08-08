3c94f4f9429647589cf9eb899f46e4963dc516a9
seanoh1014 <ohsean1014@gmail.com>
2026-08-07 21:06:14 -0500

    use compact cursor theme in niri

--- home-manager/niri/default.nix
+++ home-manager/niri/default.nix
@@ -17,6 +17,7 @@ in
     nerd-fonts.fira-code
     swaybg
     swaylock
+    vanilla-dmz
     waybar
     wl-clipboard
     xwayland-satellite
@@ -47,7 +48,7 @@ in
 
       // Match the compact classic cursor used by the X11/DWM session.
       cursor {
-          xcursor-theme "default"
+          xcursor-theme "DMZ-White"
           xcursor-size 16
       }
 
